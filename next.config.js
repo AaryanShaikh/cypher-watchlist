55ed326d59b6019d78c75d1e3c7d4ac0c8d6d50d
Aaryan Shaikh <aayanshaikh27638@gmail.com>
2023-05-11 15:39:04 +0530

    test with gh-pages b4 deploy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const withGitHubPages = require('next-gh-pages');
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = withGitHubPages({
+    basePath: '/cypher-watchlist', // Replace <repository-name> with your GitHub repository name
+  });
