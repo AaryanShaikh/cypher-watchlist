c89e3e4c3080e2d7a23b18cb3cd784a356e262f0
Aaryan Shaikh <aayanshaikh27638@gmail.com>
2023-05-11 15:53:04 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
-const withGitHubPages = require('gh-pages');
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-module.exports = withGitHubPages({
-    basePath: '/cypher-watchlist', // Replace <repository-name> with your GitHub repository name
-});
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+    const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+    assetPrefix = `/${repo}/`
+    basePath = `/${repo}`
+}
+
+module.exports = {
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+}
