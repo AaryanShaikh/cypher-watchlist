8ea7c65352e51d5f2d9b0c98e39620fa00c4f517
Aaryan Shaikh <aayanshaikh27638@gmail.com>
2023-05-12 16:37:59 +0530

    added cards and some series

---
+++ components/Card.js
@@ -0,0 +1,31 @@
+import { Typography, Tag, Space, Tooltip } from 'antd'
+import React from 'react'
+import { FaPlayCircle } from 'react-icons/fa'
+import { BsFillPatchCheckFill } from 'react-icons/bs'
+
+const { Text } = Typography
+
+const CardItem = ({ range, show, imgSrc, title, eps, total, type, category, status }) => {
+    return (
+        <div className={show ? "" : "removeCard"} style={{ boxShadow: "0px 0px 8px 2px #5454543d", height: range ? "300px" : "355px", width: range ? "" : "205px", borderRadius: "10px", overflow: "hidden", padding: "5px", display: "flex", justifyContent: "space-between", flexDirection: "column", transition: ".5s ease-in-out" }}>
+            <img
+                src={imgSrc}
+                alt='img'
+                style={{ height: "80%", width: "100%" }}
+            />
+            <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
+                <Space size="">
+                    <Tag color="green">{eps}</Tag>
+                    <Tag color="purple">{total}</Tag>
+                </Space>
+                <Tooltip title={status == "in progress" ? "Currently Watching" : "Completed"}>
+                    {status == "in progress" ? <FaPlayCircle style={{ color: "#389DEF" }} /> : <BsFillPatchCheckFill style={{ color: "#78F100" }} />}
+                </Tooltip>
+                <Text strong type="secondary">{type}</Text>
+            </div>
+            <Text>{title}</Text>
+        </div>
+    )
+}
+
+export default CardItem
--- pages/homepage/Homepage.js
+++ pages/homepage/Homepage.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react'
-import { Input, Typography } from 'antd'
+import { Input, Menu, Typography } from 'antd'
+import { GiPopcorn, GiAbstract060, GiTv } from 'react-icons/gi';
+import { BiCameraMovie, BiMoviePlay } from 'react-icons/bi'
+import CardItem from '../../components/Card';
+import rawData from "../../data/rawData.json"
 
 const { Text } = Typography
 const { Search } = Input
@@ -7,17 +11,43 @@ const { Search } = Input
 const Homepage = () => {
     const [mainwidth, setwidth] = useState("")
     const widthRef = useRef(null)
-    const range = mainwidth > 300 && mainwidth < 500 
+    const range = mainwidth > 300 && mainwidth < 500
+    const [categorySel, setcategorySel] = useState("all")
+    const webListStyle = { display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))", width: "98%", gap: "20px", overflowY: "scroll", overflowX: "hidden", maxHeight: "85vh", padding: "10px 5px", transition: ".5s ease-in-out" }
+    const mobListStyle = { display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(150px, 1fr))", width: "98%", gap: "20px", overflowY: "scroll", overflowX: "hidden", maxHeight: "65vh", padding: "10px 5px" }
 
     useEffect(() => {
         setwidth(widthRef.current.offsetWidth)
     }, [widthRef])
 
+
+
     return (<>
         <div ref={widthRef} style={{ position: "absolute", width: "100%" }}></div>
         <div className='heading'>
             <Text className='logo'>{range ? "CW" : "Cypher's WatchList"}</Text>
-            <Search style={{ width: range ? "55%" : "25%" }} placeholder='Try searching Anime, Series or Movie name' />
+            <Search style={{ width: range ? "55%" : "25%" }} placeholder={range ? "Search here" : "Try searching Anime, Series or Movie name"} />
+        </div>
+        <div style={{ display: "flex", flexDirection: range ? "column" : "row", gap: "5px" }}>
+            <Menu
+                style={{ width: range ? "100%" : "150px" }}
+                mode={range ? "horizontal" : "inline"}
+                inlineCollapsed={false}
+                selectedKeys={categorySel}
+            >
+                <Menu.Item key="all" onClick={() => setcategorySel("all")} icon={<GiPopcorn style={{ fontSize: "20px" }} />}>{range ? "" : "All"}</Menu.Item>
+                <Menu.Item key="ongoing" onClick={() => setcategorySel("ongoing")} icon={<GiTv style={{ fontSize: "20px" }} />}>{range ? "" : "Ongoing"}</Menu.Item>
+                <Menu.Item key="anime" onClick={() => setcategorySel("anime")} icon={<GiAbstract060 style={{ fontSize: "20px" }} />}>{range ? "" : "Anime"}</Menu.Item>
+                <Menu.Item key="series" onClick={() => setcategorySel("series")} icon={<BiMoviePlay style={{ fontSize: "20px" }} />}>{range ? "" : "Series"}</Menu.Item>
+                <Menu.Item key="movies" onClick={() => setcategorySel("movies")} icon={<BiCameraMovie style={{ fontSize: "20px" }} />}>{range ? "" : "Movies"}</Menu.Item>
+            </Menu>
+            <div className='scrollVisible' style={range ? mobListStyle : webListStyle}>
+                {
+                    rawData.map((ele, ind) => {
+                        return <CardItem key={ind} range={range} show={(ele.status=="in progress" && categorySel=="ongoing")|| categorySel == "all" || ele.category == categorySel} imgSrc={ele.imgSrc} title={ele.title} eps={ele.eps} total={ele.total} type={ele.type} status={ele.status} />
+                    })
+                }
+            </div>
         </div>
     </>
     )
