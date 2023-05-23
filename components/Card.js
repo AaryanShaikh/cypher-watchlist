34a718cfb9bd06a9e773936af8b17cb63b8d64ca
Aaryan Shaikh <aayanshaikh27638@gmail.com>
2023-05-23 13:54:24 +0530

    added loader for the images

--- components/Card.js
+++ components/Card.js
@@ -10,11 +10,17 @@ const CardItem = ({ range, show, imgSrc, title, eps, total, type, category, stat
 
     return (
         <div style={{ boxShadow: "0px 0px 8px 2px #5454543d", height: show ? range ? "300px" : "355px" : "0px", width: show ? range ? "" : "205px" : "0px", borderRadius: "10px", overflow: "hidden", padding: "5px", display: "flex", justifyContent: "space-between", flexDirection: "column", transition: ".5s ease", opacity: "1", position: "relative", background: dark ? "#3A3A3A" : "white" }}>
-            <img
-                src={imgSrc}
-                alt='Loading Badass Image...'
-                style={{ height: "80%", width: "100%" }}
-            />
+            <div style={{ height: "80%", width: "100%", position: "relative" }}>
+                <div style={{ height: "100%", width: "100%", position: "absolute", opacity: isImgLoading ? "1" : "0", display: "flex", justifyContent: "center", alignItems: "center", transition: ".5s ease-in-out" }}>
+                    <div class="spinner"></div>
+                </div>
+                <img
+                    src={imgSrc}
+                    alt='Loading Badass Image...'
+                    style={{ height: "100%", width: "100%", opacity: isImgLoading ? "0" : "1", borderTopLeftRadius: "10px", borderTopRightRadius: "10px", transition: ".5s ease-in-out" }}
+                    onLoad={() => setisImgLoading(false)}
+                />
+            </div>
             <div style={{ display: "flex", alignItems: "center", justifyContent: "space-between" }}>
                 <Space size="">
                     <Tag color="green">{eps}</Tag>
