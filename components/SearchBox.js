d90fd3e94023882e870275043846c9059f090c0e
Aaryan Shaikh <aayanshaikh27638@gmail.com>
2023-06-27 20:54:21 +0530

    updated search box to 2.0

--- components/SearchBox.js
+++ components/SearchBox.js
@@ -1,7 +1,18 @@
 import React, { useEffect, useState } from 'react'
+import { BiSearchAlt } from 'react-icons/bi';
 
 const SearchBox = ({ searchText, setsearchText, isActive, setisActive, dark, refs }) => {
     const [text, settext] = useState("")
+    const [labelText, setlabelText] = useState("Seek & Find...")
+    const [isInputFocused, setIsInputFocused] = useState(false);
+
+    const handleInputFocus = () => {
+        setIsInputFocused(true);
+    };
+
+    const handleInputBlur = () => {
+        setIsInputFocused(false);
+    };
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -14,11 +25,28 @@ const SearchBox = ({ searchText, setsearchText, isActive, setisActive, dark, ref
 
     return (<>
         <div ref={refs} style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
-            <div onClick={() => { isActive ? settext("") : "" }} style={{ height: "30px", width: "30px", border: `1px solid ${dark ? "white" : "black"}`, borderRight: dark ? "white" : "#0e0e0e", justifyContent: "center", alignItems: "center", display: "flex", position: "relative", borderTopLeftRadius: isActive ? "0px" : "10px", borderBottomLeftRadius: isActive ? "0px" : "10px", transition: ".5s ease-in", cursor: isActive ? "pointer" : "auto" }}>
-                <div style={{ height: "10px", width: "10px", borderRadius: isActive ? "0%" : "50%", border: `solid ${dark ? "white" : "black"}`, position: "absolute", top: isActive ? "7px" : "4px", transition: ".5s ease-in", transform: `rotate(${isActive ? 40 : 0}deg)`, borderTop: isActive ? `${dark ? "white" : "black"}` : `solid ${dark ? "white" : "black"}`, borderRight: isActive ? `${dark ? "white" : "black"}` : `solid ${dark ? "white" : "black"}` }}></div>
-                <div style={{ height: "7px", width: "10px", border: `solid ${dark ? "white" : "black"}`, position: "absolute", borderRight: `${dark ? "white" : "black"}`, borderBottom: `${dark ? "white" : "black"}`, borderTop: `${dark ? "white" : "black"}`, right: isActive ? "4px" : "-2px", transition: ".5s ease-in", transform: `rotate(${isActive ? 90 : 327}deg)`, bottom: isActive ? "8px" : "9px" }}></div>
+            <BiSearchAlt style={{ color: dark ? "#fff" : "#000", transition: ".5s cubic-bezier(0.68, -0.55, 0.265, 1.55)" }} />
+            <div style={{ height: "30px", width: isInputFocused || text != "" ? "190px" : "100px", border: "solid #999", borderBottomRightRadius: "20px", borderTop: "transparent", borderLeft: "transparent", borderRight: "transparent", position: "relative", padding: "0px 5px", transition: ".5s cubic-bezier(0.68, -0.55, 0.265, 1.55)", borderImage: `linear-gradient(to right, ${dark ? "#999" : "#000"}, ${dark ? "#222222" : "#fff"}) 1` }}>
+                <div style={{ position: "absolute", display: "flex", justifyContent: 'flex-start', width: "100%" }}>
+                    <div className='searchLabel'>
+                        {
+                            labelText.split('').map((ele, ind) => {
+                                return <span className={`textFont ${isInputFocused || text != "" ? "searchTxt" : ""}`} style={{ transitionDelay: `${ind * 40}ms`, color: dark ? "#fff" : "#000" }} key={ind}>{ele}</span>
+                            })
+                        }
+                    </div>
+                </div>
+                <input
+                    value={text}
+                    onChange={(e) => settext(e.target.value)}
+                    onFocus={handleInputFocus}
+                    onBlur={handleInputBlur}
+                    style={{ background: "none", outline: "none", border: "none", height: "100%", width: "90%", position: "absolute", color: dark ? "aliceblue" : "#000" }}
+                />
             </div>
-            <div><input placeholder='Seek and find...' value={text} onChange={(e) => settext(e.target.value)} className="searchBx textFont" style={{ height: "30px", width: "200px", border: `1px solid ${dark ? "white" : "black"}`, padding: 0, margin: 0, borderLeft: dark ? "white" : "#0e0e0e", borderTopRightRadius: isActive ? "0px" : "10px", borderBottomRightRadius: isActive ? "0px" : "10px", transition: ".5s ease-in", color: dark ? "aliceblue" : "black" }} /></div>
+            {/* <div>
+                <input placeholder='Seek and find...' value={text} onChange={(e) => settext(e.target.value)} className="searchBx textFont" style={{ height: "30px", width: "200px", border: `1px solid ${dark ? "white" : "black"}`, padding: 0, margin: 0, borderLeft: dark ? "white" : "#0e0e0e", borderTopRightRadius: isActive ? "0px" : "10px", borderBottomRightRadius: isActive ? "0px" : "10px", transition: ".5s ease-in", color: dark ? "aliceblue" : "black" }} />
+            </div> */}
         </div>
     </>
     )
